173ca831a6dec3b624c421116db1409eee801cff
marcus <38634617+vimona3ds@users.noreply.github.com>
2024-10-06 19:22:57 -0700

    randomly generate grids for now

--- src/index.ts
+++ src/index.ts
@@ -4,33 +4,38 @@ import { isGameElements } from './utils/isGameElements';
 import { GameGrid } from './gameGrid';
 import "./index.scss";
 
+const threeLetterWords = ["bat", "cat", "sat", "fat", "ate", "eat", "but", "act", "set", "run", "far", "car", "rat", "set"];
+const twoLetterWords = ["at", "be", "do", "go", "he", "in", "it", "me", "my", "no", "of", "on", "or", "so", "to", "up", "us", "we"];
+
+const randomElementFrom = (array: string[]) => array[Math.floor(Math.random() * array.length)];
+
 const config: GameConfig = {
   id: 0,
   rows: 3,
   cols: 3,
   tokens: [
     createToken({
-      content: "ace",
+      content: randomElementFrom(twoLetterWords),
       layout: {
         type: "direction",
-        initialPosition: { x: 0, y: 0 },
+        initialPosition: { x: 0, y: 1 },
         direction: { x: 1, y: 1 },
       },
     }),
     createToken({
-      content: "at",
+      content: randomElementFrom(threeLetterWords),
       layout: {
         type: "direction",
-        initialPosition: { x: 1, y: 0 },
-        direction: { x: 1, y: 0 },
+        initialPosition: { x: 2, y: 2 },
+        direction: { x: -1, y: -1 },
       },
     }),
     createToken({
-      content: "b",
+      content: randomElementFrom(twoLetterWords),
       layout: {
         type: "direction",
-        initialPosition: { x: 2, y: 1 },
-        direction: { x: 1, y: 0 },
+        initialPosition: { x: 1, y: 0 },
+        direction: { x: 1, y: 1 },
       },
     }),
   ]
