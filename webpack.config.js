00a4649c5d987d988c83ba5deea2763fb3af3bcb
marcus <38634617+vimona3ds@users.noreply.github.com>
2024-10-06 19:36:42 -0700

    add mini css extract plugin, rebuild

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
@@ -16,8 +17,9 @@ module.exports = {
       {
         test: /\.s[ac]ss$/i,
         use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
+          MiniCssExtractPlugin.loader,
+          // // Creates `style` nodes from JS strings
+          // "style-loader",
           // Translates CSS into CommonJS
           "css-loader",
           // Compiles Sass to CSS
@@ -31,6 +33,7 @@ module.exports = {
     watchFiles: ['src/**/*', 'static/**/*'],
   },
   plugins: [
+    new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
       template: "./static/index.html",
     }),
